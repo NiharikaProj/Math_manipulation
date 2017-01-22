6f121934e267b2ad022d51d9804b0957e33f3ce7
Niharika <niharikageneral@gmail.com>
2017-01-23 00:37:09 +0530

    updated

--- AddOneToNumber.java
+++ AddOneToNumber.java
@@ -5,17 +5,14 @@ public class AddOneToNumber {
 	public static void main(String[] args) {
 	 	
 		List<Integer> Value = new ArrayList<Integer>();
-		Value.add(9);
-		Value.add(9);
-		Value.add(9);
+		//Value.add(0);
+		//Value.add(0);
+		//Value.add(6);
 		Value.add(9);
 		
 		System.out.println("Input array: " + Value);
 		List<Integer> ArraynewValue = plusOne((ArrayList<Integer>) Value);
-		if(ArraynewValue.get(0) == 0)
-		{
-			ArraynewValue.remove(0);
-		}
+		
 		System.out.println("Output array: "+ ArraynewValue);	
 			
 	}	
@@ -28,8 +25,13 @@ public class AddOneToNumber {
 			int currval=value.get(0);
 			currval=currval+1;
 			value.set(0, currval);
+			if(value.get(0)==10)
+			{
+				value.add(0, 1);
+				value.set(1, 0);
+			}
 		}
-		if (length>2)
+		if (length>=2)
 		{
 		int nextVal = value.get(length-2);;
 		int lastVal = value.get(length-1);
@@ -45,6 +47,7 @@ public class AddOneToNumber {
 			value.set(length-1, 0);
 			value.set(length-2, nextVal);
 		}
+
 		
 for (int i =length-1; i>=0; i--)
 {
@@ -62,18 +65,17 @@ for (int i =length-1; i>=0; i--)
 				nextval=nextval+1;
 				value.set(i-1, nextval);
 			}
-		
+
 	
 }	 
-		}
-
-if (value.get(0)==10)
+for(int j=0;j<length-1;j++)
 {
-	value.add(0, 1);
-	value.set(1, 0);
-
+	while (value.get(j)==0)
+		value.remove(j);
+		break;
 }
-		
+	
+		}	
 	
 		return value;
 	}
