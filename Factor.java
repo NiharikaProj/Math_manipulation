f9832abf2022327224e6831909797bd1e98552c2
Niharika <niharikageneral@gmail.com>
2017-01-29 16:21:27 +0530

    update

--- Factor.java
+++ Factor.java
@@ -2,12 +2,17 @@
 public class Factor {
 
 	public static void main(String[] args) {
-		int n = 6, i = 0;
+		int n = 24, i = 0;
 		System.out.println("factor of: " + n);
 		System.out.println("1");
-		for (i = 2; i < n; i++) {
-			if (n % i == 0)
+		for (i = n / 2; i > 1; i--) {
+			if (n % i == 0 && (i>(n/i))) {
+
 				System.out.println(i);
+				System.out.println(n / i);
+
+			}
+
 		}
 
 		System.out.println(n);
